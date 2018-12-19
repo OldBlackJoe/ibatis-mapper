378ad750264999b471f23909b8fd8a72899140c9
Kim KyoungDong <kd52.kim@samsung.com>
2018-12-19 17:57:45 +0900

    Add close, open, prepend in convertBinaryCondition

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ ibatisMapper.prototype.getStatement = function(namespace, sql, param, format) {
   try{
     for (var i = 0, children; children = iBatisMapper[namespace][sql][i]; i++) {
       // Convert SQL statement recursively
-      statement += convert.convertChildren(children, param, namespace, iBatisMapper);
+      statement += convert.convertChildren(children, param, namespace, iBatisMapper, false);
     }
     
     // Check not converted Parameters
